0f789cae5127a2b5ef11a182f24a8ff2892b7597
seiyakanaoka <ttf.1998.seiya@gmail.com>
2021-11-14 00:26:59 +0900

    継承と抽象クラスとシングルトンパターンについて

--- compile/class.js
+++ compile/class.js
@@ -1,17 +1,53 @@
 "use strict";
 class Person {
-    constructor(initName) {
-        this.name = initName;
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    static isAdult(age) {
+        if (age > 17)
+            return true;
+        return false;
+    }
+    showName() {
+        console.log(this.name);
+    }
+    showAge() {
+        console.log(this.age);
+    }
+    incrementAge() {
+        this.age += 1;
     }
     greeting() {
-        console.log(`Hello! My name is ${this.name}`);
+        console.log(`Hello! My name is ${this.name}. I am ${this.age} years old.`);
+        this.explainJob();
     }
 }
-const quill = new Person('Quill');
-console.log(quill);
-quill.greeting();
-const anotherQuill = {
-    name: 'anotherQuill',
-    anotherGreeting: quill.greeting
-};
-anotherQuill.anotherGreeting();
+Person.species = 'Homo sapiens';
+class Teacher extends Person {
+    constructor(name, age, _subject) {
+        super(name, age);
+        this._subject = _subject;
+    }
+    explainJob() {
+        console.log(`I am a teacher and I teach ${this.subject}.`);
+    }
+    get subject() {
+        if (!this._subject) {
+            throw new Error('There is no subject.');
+        }
+        return this._subject;
+    }
+    set subject(value) {
+        this._subject = value;
+    }
+    static getInstance() {
+        if (Teacher.instance)
+            return Teacher.instance;
+        Teacher.instance = new Teacher('Quill', 38, 'Math');
+        return Teacher.instance;
+    }
+}
+const teacher = Teacher.getInstance();
+const teacher2 = Teacher.getInstance();
+console.log(teacher, teacher2);
--- src/class.ts
+++ src/class.ts
@@ -1,19 +1,50 @@
-class Person {
-  name: string;
-  constructor(initName: string) {
-    this.name = initName;
+abstract class Person {
+  static species = 'Homo sapiens';
+  static isAdult(age: number) {
+    if (age > 17) return true;
+    return false;
   }
+  constructor(protected name: string, protected age: number) {
+  }
+  showName() {
+    console.log(this.name);
+  }
+  showAge() {
+    console.log(this.age);
+  }
+  incrementAge() {
+    this.age += 1;
+  }
+  greeting(this: Person) {
+    console.log(`Hello! My name is ${this.name}. I am ${this.age} years old.`);
+    this.explainJob();
+  }
+  abstract explainJob(): void;
+}
 
-  greeting(this: { name: string}) {
-    console.log(`Hello! My name is ${this.name}`);
+class Teacher extends Person {
+  private static instance: Teacher;
+  explainJob() {
+    console.log(`I am a teacher and I teach ${this.subject}.`);
+  }
+  get subject() {
+    if (!this._subject) {
+      throw new Error('There is no subject.');
+    }
+    return this._subject;
+  }
+  set subject(value) {
+    this._subject = value;
+  }
+  private constructor(name: string, age: number, private _subject: string) {
+    super(name, age);
+  }
+  static getInstance() {
+    if (Teacher.instance) return Teacher.instance;
+    Teacher.instance = new Teacher('Quill', 38, 'Math');
+    return Teacher.instance;
   }
 }
-const quill = new Person('Quill');
-console.log(quill);
-quill.greeting();
-
-const anotherQuill = {
-  name: 'anotherQuill',
-  anotherGreeting: quill.greeting
-}
-anotherQuill.anotherGreeting();
+const teacher = Teacher.getInstance();
+const teacher2 = Teacher.getInstance();
+console.log(teacher, teacher2);
